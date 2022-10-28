5c6161e3f65f822fcc8e27fdeae8dabb57134b56
Bernard Lambeau <blambeau@gmail.com>
2022-10-28 16:18:29 +0200

    Initial commit that mimics printit-rb with weasyprint.

--- src/types.ts
+++ src/types.ts
@@ -4,6 +4,11 @@ export type Config = {
   logger: Logger
 }
 
+export type PrintInput = {
+  html: String
+  attachment ?: String
+}
+
 declare global {
   namespace Express {
     interface Request {
