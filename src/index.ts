92e8f1c7f7174d3ac170a68c1c2cfa3bf239c7c0
Bernard Lambeau <blambeau@gmail.com>
2022-10-28 16:54:02 +0200

    Add support for the same config as printit-rb 1.x.

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,24 @@
 import * as pack from '../package.json';
 import logger from './logger';
+import fs from 'fs';
+import { createApp } from './api';
+const yaml = require('js-yaml');
 
 const { version } = pack;
 export { version };
 
 export * from './errors';
 
-import { createApp } from './api';
+try {
+  const data = yaml.load(fs.readFileSync('config/printit.yml', 'utf8'));
+  const config = Object.assign(data, {
+    logger: logger,
+  });
+  const app = createApp(config);
 
-const config = {
-  logger: logger,
-};
-const app = createApp(config);
-
-app.listen(3000, () => {
-  console.log('Printit is not listening on http://0.0.0.0:3000');
-});
+  app.listen(3000, () => {
+    console.log('Printit is now listening on http://0.0.0.0:3000');
+  });
+} catch (e) {
+  console.log(e);
+}
---
+++ tests/lib/weasyprint/params.spec.ts
@@ -0,0 +1,14 @@
+import chai from 'chai';
+import { expect } from 'chai';
+import sinonChai from 'sinon-chai';
+chai.use(sinonChai);
+
+import { WeasyprintParams } from '../../../src/lib/Weasyprint';
+
+describe('WeasyprintParams#toArgs', () => {
+  const wsp = new WeasyprintParams({ m: 'print' });
+
+  it('works', () => {
+    expect(wsp.toArgs()).to.eql(['-m', 'print', '-', '-']);
+  });
+});
